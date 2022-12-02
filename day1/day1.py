96a7e69725779f513454bc40ffde2549cf8d7854
Gabby <vinxa17@gmail.com>
2022-12-02 10:52:12 +0800

    day1_part2

--- day1/day1.py
+++ day1/day1.py
@@ -13,6 +13,13 @@ with open(os.path.join(os.path.dirname(__file__),input)) as f:
             elf_sum = 0
 
 elf_calories = [(i+1, x) for i, x in enumerate(elf_calories)]
-elf_calories = sorted(elf_calories, reverse=True, key=lambda i:i[1])[0]
 
-print(f'The elf with the most calories is the {elf_calories[0]}th elf which has {elf_calories[1]} calories.')
+
+# Part 1
+elf_most_calories = sorted(elf_calories, reverse=True, key=lambda i:i[1])[0]
+print(f'The elf with the most calories is the {elf_most_calories[0]}th elf which has {elf_most_calories[1]} calories.')
+
+#Part 2
+top_three_calories = sorted(elf_calories, reverse=True, key=lambda i:i[1])[0:3]
+print(top_three_calories)
+print(f'The top three elves with the most calories are the {top_three_calories[0][0]}th, {top_three_calories[1][0]}th and {top_three_calories[2][0]}th elf which have {sum(amount for index,amount in top_three_calories)} calories combined.')
